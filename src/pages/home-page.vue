8a0cf937d537ec1ec7d192212643917b02f5b7fe
binhphung <binhphung18032000@gmail.com>
2022-05-13 12:01:30 +0700

    updated setup in API 130522

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,44 +1,55 @@
 <template>
   <h1>Overview page is here</h1>
-  <!-- <p>{{ firstName }}</p> -->
-  <p>{{ secondName }}</p>
-  <p>{{ car }}</p>
-  <button @click="onChangeSomeThing">Click me!</button>
+  <input type="text" v-model="searchText" />
+  <ul>
+    <li v-for="(customer, index) in customersFiltered" :key="index">
+      {{ customer }}
+    </li>
+  </ul>
 </template>
 <script>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed, watch, watchEffect } from "vue";
 export default {
-  setup() {
-    const firstName = ref("Binh Phung");
-    const secondName = ref({
-      name: "Hoa",
-      something: "female",
-    });
-    let car = reactive({
-      price: 650000,
-      name: "Mitsumitsi",
+  props: {
+    theme: {
+      type: String,
+      required: false,
+      default: "light",
+    },
+  },
+  // eslint-disable-next-line vue/no-setup-props-destructure
+  setup({ theme }, { emit }) {
+    // console.log(props);
+    // console.log(context);
+    console.log(theme);
+    console.log(emit);
+
+    const searchText = ref("");
+    const customers = reactive([
+      "Anh Tran",
+      "Duy Nguyen",
+      "Binh Phung",
+      "Duc Nguyen",
+      "Thinh Pham",
+    ]);
+    const customersFiltered = computed(() =>
+      customers
+        .map((customer) => {
+          customer = customer.toLowerCase();
+          return customer;
+        })
+        .filter((customer) => customer.includes(searchText.value.toLowerCase()))
+    );
+    watch(searchText, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
     });
 
-    function onChangeSomeThing() {
-      // console.log("Running here...");
-      // firstName.value = "Phùng An Bình";
-      // secondName.value.name = "Hoa thi";
-      // car.price = 860000;
-      // console.log(firstName);
-      // console.log(secondName);
-      // console.log(car);
-      secondName.value = {
-        name: "Nguyen Phu",
-        something: "male",
-      };
-      car = {
-        price: 500000,
-        something: "Toyota",
-      };
-      console.log(secondName);
-      console.log(car);
-    }
-    return { firstName, secondName, car, onChangeSomeThing };
+    watchEffect(() => {
+      if (searchText.value) {
+        console.log("Running again...");
+      }
+    });
+    return { searchText, customersFiltered };
   },
 };
 </script>
--- src/pages/transactions-page.vue
+++ src/pages/transactions-page.vue
@@ -14,19 +14,38 @@
       </div>
     </div>
   </div>
+  <div v-else-if="error">{{ error.message }}</div>
   <div v-else>Loading transaction page</div>
 </template>
 <script>
+import { ref } from "vue";
 export default {
-  data() {
-    return {
-      transactions: [],
+  // data() {
+  //   return {
+  //     transactions: [],
+  //   };
+  // },
+  // created() {
+  //   fetch("http://localhost:3000/transactions")
+  //     .then((response) => response.json())
+  //     .then((data) => (this.transactions = data));
+  // },
+  setup() {
+    const transactions = ref([]);
+    const error = ref(null);
+    const fetchAll = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/transactions");
+        if (!response.ok) throw new Error("Something went wrong! Binhphung");
+        transactions.value = await response.json();
+      } catch (err) {
+        error.value = err;
+        console.log(error.value);
+      }
     };
-  },
-  created() {
-    fetch("http://localhost:3000/transactions")
-      .then((response) => response.json())
-      .then((data) => (this.transactions = data));
+    fetchAll();
+
+    return { error, transactions };
   },
 };
 </script>
